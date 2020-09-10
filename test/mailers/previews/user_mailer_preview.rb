811f335d2a6388f1a683f8da5ece28bc4500be58
mashiken <tatsumikogorou@gmail.com>
2020-09-10 14:45:35 +0000

    Finish password-reset

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -12,7 +12,10 @@ class UserMailerPreview < ActionMailer::Preview
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
   # https://df853a45f2c44c66a30cd5bb034e34a5.vfs.cloud9.ap-northeast-1.amazonaws.com/rails/mailers/user_mailer/password_reset
   def password_reset
-    UserMailer.password_reset
+    user = User.first
+    user.reset_token = User.new_token
+    UserMailer.password_reset(user)
   end
-
+  # railsのメールプレビュー機能 パスワード再設定のメールをプレビュー
+  
 end
